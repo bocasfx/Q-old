e37de8db8915b593bdb67972568bba25d4964900
bocasfx <bocasfx@gmail.com>
2013-05-23 18:19:40 -0400

    App settings skeleton working

--- Q/SCConstants.m
+++
@@ -1,6 +0,0 @@
-NSInteger const NO_TOOL_SELECTED        = -1;
-NSInteger const CREATE_NODE_BUTTON      = 0;
-NSInteger const CREATE_STREAM_BUTTON    = 1;
-NSInteger const SETTINGS_BUTTON         = 2;
-NSInteger const LINK_NODES_BUTTON       = 3;
-NSInteger const PLAY_PAUSE_BUTTON       = 4;
