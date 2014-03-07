6c9586217ad477691b035c8b897f2c290dc5443d
BocasFx <bocasfx@gmail.com>
2014-03-07 10:39:17 -0500

    Settings popup

--- Kivy/Main.py
+++ Kivy/Main.py
@@ -3,18 +3,16 @@ from Medium import Medium
 from Transport import Transport
 from kivy.uix.floatlayout import FloatLayout
 from ToolsPanel import ToolsPanel
-# from ControlPanel import ControlPanel
+from MainSettings import MainSettings
 from kivy.core.window import Window
 from kivy.logger import Logger
-# from QSettingsPanel import QSettingsPanel
-from kivy.uix.popup import Popup
-from kivy.uix.label import Label
-
 
 class QApp(App):
 
     def __init__(self, **kwargs):
         super(QApp, self).__init__(**kwargs)
+
+        # Initialize keyboard handler.
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
 
@@ -22,14 +20,15 @@ class QApp(App):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard
 
+    # Keyboard handler
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         Logger.debug('Key pressed: ' + str(keycode))
         if keycode[1] == 'spacebar':
             self.transport.on_spacebar_pressed()
         elif keycode[1] == 'k':
             self.medium.clear()
-        elif keycode[1] == 'c':
-            self.show_settings()
+        elif keycode[1] == 's':
+            self.mainSettings.toggle()
         elif keycode[1] == 't':
             self.toggle_panel(self.tools_panel)
         elif keycode[1] == 'p':
@@ -44,11 +43,8 @@ class QApp(App):
             self.main_layout.remove_widget(panel)
             panel.visibility = 'hidden'
 
-    def show_settings(self):
-        self._popup = Popup(title="Settings", content=Label(text='Settings'), size_hint=(0.9, 0.9))
-        self._popup.open()
-
     def build(self):
+        self.mainSettings = MainSettings()
         self.medium = Medium(do_translation=False)
         self.tools_panel = ToolsPanel(medium=self.medium)
         self.tools_panel.visibility = 'visible'
